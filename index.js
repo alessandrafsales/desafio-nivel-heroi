26aae27f2a357b4ec4f12499f836fd94efe1304b
alessandrafsales <alessandrasales.tech@gmail.com>
2025-02-13 09:57:45 +0000

    organização do código

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const congrat = "Parabéns, você alcançou o level "
 
 // Condições
 if(xp <= 1000){
-  console.log("Olá " + nomeHeroi + " você está no nível de Ferro. ")
-  level = "Ferro"
+    console.log("Olá " + nomeHeroi + " você está no nível de Ferro. ")
+    level = "Ferro"
 }
 else if(xp <= 2000){
     console.log(congrat + "Bronze!")
@@ -19,29 +19,29 @@ else if(xp <= 2000){
     console.log("O herói " + nomeHeroi + " está no nível de " + level)
 }
 else if(xp <= 5000){
-  console.log(congrat + "Prata!")
-  level = "Prata"
-  console.log("O herói " + nomeHeroi + " está no nível de " + level)
+    console.log(congrat + "Prata!")
+    level = "Prata"
+    console.log("O herói " + nomeHeroi + " está no nível de " + level)
 }
 else if(xp <= 7000){
-  console.log(congrat + "Ouro!")
-  level = "Ouro"
-  console.log("O herói " + nomeHeroi + " está no nível de " + level)
+    console.log(congrat + "Ouro!")
+    level = "Ouro"
+    console.log("O herói " + nomeHeroi + " está no nível de " + level)
 }
 else if(xp <= 8000){
-  console.log(congrat + "Platina!")
-  level = "Platina"
-  console.log("O herói " + nomeHeroi + " está no nível de " + level)
+    console.log(congrat + "Platina!")
+    level = "Platina"
+    console.log("O herói " + nomeHeroi + " está no nível de " + level)
 }
 else if(xp <= 9000){
-  console.log(congrat + "Ascendente!")
-  level = "Ascendente"
-  console.log("O herói " + nomeHeroi + " está no nível de " + level)
+    console.log(congrat + "Ascendente!")
+    level = "Ascendente"
+    console.log("O herói " + nomeHeroi + " está no nível de " + level)
 }
 else if(xp <= 10000){
-  console.log(congrat + "Imortal!")
-  level = "Imortal"
-  console.log("O herói " + nomeHeroi + " está no nível de " + level)
+    console.log(congrat + "Imortal!")
+    level = "Imortal"
+    console.log("O herói " + nomeHeroi + " está no nível de " + level)
 }
 else{
     level = "Radiante"
